b7c460dae5c310ff6ebe97526aeafc5f5539c770
Junghyun Lee(laptop) <scimath@kaist.ac.kr>
2019-09-24 06:37:01 +0900

    Homework #2

---
+++ src/main/scala/cs320/hw02/package.scala
@@ -0,0 +1,32 @@
+package cs320
+
+import cs320._
+
+package object hw02 extends Homework02 {
+  // applies a binary numeric function on all combinations of numbers from
+  // the two input lists, and return the list of all of the results
+  def binOp(
+    op: (Int, Int) => Int,
+    ls: List[Int],
+    rs: List[Int]
+  ): List[Int] = ls match {
+    case Nil => Nil
+    case l :: rest =>
+      def f(r: Int): Int = ???
+      rs.map(f) ++ binOp(op, rest, rs)
+  }
+
+  def run(str: String): List[Int] = ???
+
+  def tests: Unit = {
+    test(run("{+ 3 7}"), List(10))
+    test(run("{- 10 {3 5}}"), List(7, 5))
+    test(run("{with {x {+ 5 5}} {+ x x}}"), List(20))
+    test(run("{min 3 4 5}"), List(3))
+    test(run("{max {+ 1 2} 4 5}"), List(5))
+    test(run("{min {1 4} {2 9} 3}"), List(1, 1, 2, 3))
+    test(run("{max {1 6} {2 5} {3 4}}"), List(3, 4, 5, 5, 6, 6, 6, 6))
+
+    /* Write your own tests */
+  }
+}
