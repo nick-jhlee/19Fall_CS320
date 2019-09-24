9a7867dcbbc4f7742a3b46ac7673f05971b9d5da
Junghyun Lee(laptop) <scimath@kaist.ac.kr>
2019-09-24 03:46:29 +0200

    Homework #2: Implementation Homework #2: Tests - version 1

--- src/main/scala/cs320/hw02/package.scala
+++ src/main/scala/cs320/hw02/package.scala
@@ -12,11 +12,30 @@ package object hw02 extends Homework02 {
   ): List[Int] = ls match {
     case Nil => Nil
     case l :: rest =>
-      def f(r: Int): Int = ???
+      def f(r: Int): Int = op(l, r)
       rs.map(f) ++ binOp(op, rest, rs)
   }
 
-  def run(str: String): List[Int] = ???
+  // Interpreter for MUWAE
+  def run(str: String): List[Int] = {
+    // (Int, Int) => Int functions for the interpreter
+    def add(l: Int, r: Int): Int = l + r
+    def sub(l: Int, r: Int): Int = l - r
+    def max(l: Int, r: Int): Int = if (l < r) r else l
+    def min(l: Int, r: Int): Int = if (l < r) l else r
+
+    def interp(muwae: MUWAE, env: Map[String, List[Int]]): List[Int] = muwae match{
+      case Num(n) => n
+      case Add(l, r) => binOp(add, interp(l, env), interp(r, env))
+      case Sub(l, r) => binOp(sub, interp(l, env), interp(r, env))
+      case With(x, i, n) => interp(n, env + (x -> interp(i, env)))
+      case Id(x) => env getOrElse(x, error(s"$x is a free identifier!"))
+      case Min(l, m, r) => binOp(min, binOp(min, interp(l, env), interp(m, env)), interp(r, env))
+      case Max(l, m, r) => binOp(max, binOp(max, interp(l, env), interp(m, env)), interp(r, env))
+    }
+
+    interp(MUWAE(str), Map())
+  }
 
   def tests: Unit = {
     test(run("{+ 3 7}"), List(10))
@@ -28,5 +47,18 @@ package object hw02 extends Homework02 {
     test(run("{max {1 6} {2 5} {3 4}}"), List(3, 4, 5, 5, 6, 6, 6, 6))
 
     /* Write your own tests */
+    // 1. Num(n)
+    test(run(""), List())
+    test(run("1"), List(1))
+    test(run("{1 2}"), List(1, 2))
+
+    // 2. Add(l, r)
+    test(run("{+ 3 {2 4}}"), List(5, 7))
+    test(run("{+ 10 {3 5}}"), List(13, 15))
+    test(run("{+ {1 2} {3 4}}"), List(4, 5, 5, 6))
+
+    // 3. Sub(l, r) --> not needed! (because it is logically equivalent to Add)
+    // 4. Complicated expressions containing With, Id
+
   }
 }
